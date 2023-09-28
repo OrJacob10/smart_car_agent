9dd1cb740a634711acfd6199454b1e15451212ca
OrJacob10 <orjacob428@gmail.com>
2023-09-28 16:10:08 +0300

    project initialization

---
+++ src/main/java/com/example/SmartCarAgent/SmartCarAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartCarAgent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCarAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCarAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SmartCarAgent/SmartCarAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmartCarAgent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCarAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
