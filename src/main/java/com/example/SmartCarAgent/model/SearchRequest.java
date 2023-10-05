01f245238bb8b5ed826394baf677956c9be928a4
OrJacob10 <orjacob428@gmail.com>
2023-10-05 19:26:27 +0300

    with SearchRequest class

---
+++ src/main/java/com/example/SmartCarAgent/model/SearchRequest.java
@@ -0,0 +1,22 @@
+package com.example.SmartCarAgent.model;
+
+import lombok.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "search")
+public class SearchRequest implements Serializable {
+    @Id
+    @Column(nullable = false, updatable = false)
+    private String searchId;
+
+    private String manufacturer;
+    private double minPrice;
+    private double maxPrice;
+    private String location;
+}
